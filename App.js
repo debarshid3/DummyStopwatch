76ecadefbc1ee840490a055aa3e7ca156f7cb2ef
Debarshi <debarshi.das@aclize.com>
2022-11-21 23:41:34 +0530

    LapInteval Working

--- App.js
+++ App.js
@@ -10,17 +10,45 @@ import { useState } from 'react';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [lapId, setLapId] = useState(0);
+  const [span, setSpan] = useState(0);
+  const [lap, setLap] = useState(0);
   const [handler, setHandler] = useState(null);
+  const [isVisible, setVisibility] = useState(null);
 
   const startCounter = () => {
     let val = setInterval(() => {
       setCount(count => count + 1)
     }, 1000);
-    setHandler(val)
+    setHandler(val);
   };
 
   const stopCounter = () => {
     clearInterval(handler);
+  };
+
+  const resetCounter = () => {
+    clearInterval(handler);
+    setCount(0);
+    setLapId(0);
+    setLap(0);
+    setSpan(0);
+    setVisibility(null);
+  };
+
+  const lapCounter = () => {
+    setVisibility("show");
+    setLapId(lapId + 1);
+    setLap(count);
+    setSpan(count-lap);
+    if(isVisible!=null)
+    {
+      return(
+        <View>
+          <Text style={{ fontSize: 30 }}>#{lapId}. Lap: {span} | Click: {lap}</Text>
+        </View>
+      );
+    }
   };
 
   const TestCounter = () => {
@@ -32,6 +60,13 @@ const App = () => {
             title="Start Counter" />
           <Button onPress={() => stopCounter()}
             title="Stop Counter" />
+          <Button onPress={() => resetCounter()}
+            title="Reset Counter" />
+          <Button onPress={() => lapCounter()}
+            title="Lap Counter" />
+        </View>
+        <View>
+          <Text style={{ fontSize: 30 }}>#{lapId}. Lap: {span} | Click: {lap}</Text>
         </View>
       </>
     );
